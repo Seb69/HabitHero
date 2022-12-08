d621fc1af715c4f336330526eaa7fbea24dfb2d0
Sebastian Laietiu <sebastian.laietiu@gmail.com>
2022-12-08 08:11:10 +0200

    Initial Commit

---
+++ HabitHero/HabitHeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitHeroApp.swift
+//  HabitHero
+//
+//  Created by Seb69 on 08.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct HabitHeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
